fd4dde0eaca696cbdaed23ce4748e962c78374d5
PEN.gu1n <pigcn@outlook.com>
2019-05-03 22:35:39 +0800

    Re-uploaded Misplaced Files

---
+++ Python Learning/Learn Python 3 The Hard Way/Exercises/Exercise 1/ex01.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print('she wouldn\'t have said "not to touch it"')
+
+# If typing 'it's' and get error, type 'it\'s' to escape apostrophe
+
+#—Octothrope/Pound/Hash/Mesh
